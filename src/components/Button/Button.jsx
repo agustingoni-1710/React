73d534a27bd461702603c050dcfe113cd0b63a23
Agustin Goni <agustingoni1710@gmail.com>
2023-02-14 23:42:24 -0300

    Primera Entrega

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Button = (props) => {
+
+  return (
+    
+    !props.isUsedBootstrap ?
+      <button 
+        style={props.styleCondition ? {backgroundColor: 'red', color: 'white'} : {backgroundColor: 'green', color: 'white'}}>
+          {props.buttonName}
+      </button>
+    :
+      <button type="button" class="btn btn-dark">{props.buttonName}</button> 
+  )
+}
+
+export default Button
